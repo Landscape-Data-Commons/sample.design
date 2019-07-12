3499fa63df65a4ae5e86f57929400625e85df425
nstauffer <ngstauffer@gmail.com>
2019-07-11 19:28:39 -0700

    Update documentation to reflect argument name changes

--- R/balance_around.R
+++ R/balance_around.R
@@ -300,13 +300,12 @@ keep_farthest <- function(existing_points,
 }
 
 
-##  Ingest inputs, call functions to access XY coords and to identify and eliminate New points, and output
-##          expanded, balanced design.
-#' @param existing_points_spdf Spatial ponits data frame. The existing points that will be balanced around.
-#' @param new_points_spdf Spatial points data frame. The points that will be compared against the existing points and selected from to create a balanced design.
-#' @param stratafield Character string. The name of the variable in common between \code{existing_points_spdf} and \code{new_points_spdf} that contains stratum identities. This is used to balance by stratum. If \code{NULL} then balancing will not take strata into account. Defaults to \code{NULL}.
+#' Combine existing and new points to create a spatially balanced design
+#' @param existing_points Spatial ponits data frame. The existing points that will be balanced around.
+#' @param new_points Spatial points data frame. The points that will be compared against the existing points and selected from to create a balanced design.
+#' @param stratafield Character string. The name of the variable in common between \code{existing_points} and \code{new_points} that contains stratum identities. This is used to balance by stratum. If \code{NULL} then balancing will not take strata into account. Defaults to \code{NULL}.
 #' @param projection CRS object. The projection to force on the spatial objects. Defaults to \code{sp::CRS("+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0")}.
-#' @return A spatial points data frame containing all the points from \code{existing_points_spdf} and the selected points from \code{new_points_spdf}. The projection will match \code{projection}.
+#' @return A spatial points data frame containing all the points from \code{existing_points} and the selected points from \code{new_points}. The projection will match \code{projection}.
 #' @export
 balance_around <- function(existing_points,
                            new_points,
