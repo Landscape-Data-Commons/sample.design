8fa7cbd914a1ced7384ea9c97b6454f614feed4d
nstauffer <ngstauffer@gmail.com>
2020-11-23 12:59:04 -0800

    Specify only the relevant variables
    Avoids issues with mismatch variable orders/counts

--- R/balance_around.R
+++ R/balance_around.R
@@ -40,8 +40,8 @@ find_preferences <- function(template_points,
                                   y_var = "YMETERS",
                                   projection = sp::CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"))
 
-  distance_matrix <- dist_matrix(dataframe = rbind(template_points@data,
-                                      comparison_points@data),
+  distance_matrix <- dist_matrix(dataframe = rbind(template_points@data[, c("XMETERS", "YMETERS")],
+                                      comparison_points@data[, c("XMETERS", "YMETERS")]),
                      x_var = "XMETERS",
                      y_var = "YMETERS")
 
