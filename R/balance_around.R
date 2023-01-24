e2772c02b2fce32c425f7a06c28780133c0dec06
Nelson Stauffer <ngstauffer@gmail.com>
2023-01-24 08:28:00 -0800

    Force attributes to be a data frame

--- R/balance_around.R
+++ R/balance_around.R
@@ -483,7 +483,7 @@ keep_farthest <- function(existing_points,
                                 projection = "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs")
   
   # Get a distance matrix
-  distance_matrix <- dist_matrix(dataframe = sf::st_drop_geometry(combined_points),
+  distance_matrix <- dist_matrix(dataframe = as.data.frame(sf::st_drop_geometry(combined_points)),
                                  x_var = "XMETERS",
                                  y_var = "YMETERS")
   
